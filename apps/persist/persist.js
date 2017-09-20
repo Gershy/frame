13072356892734caa2f25a3a400d3397d2c80a86
Gershom Maes <gershom.maes@gmail.com>
2017-09-20 16:51:56 -0400

    better persister $ensureFile

--- apps/persist/persist.js
+++ apps/persist/persist.js
@@ -19,8 +19,37 @@ new PACK.pack.Package({ name: 'persist',
       }});
     };
     var $ensureDir = function(pathName) {
-      return $readDir(pathName).fail(function() { return $createDir(pathName); }).then(function() {
+      
+      var $ret = p.$null;
+      
+      var pcs = pathName.split(path.sep);
+      var curPathName = pcs[0];
+      
+      for (var i = 1, len = pcs.length; i < len; i++) {
+        
+        curPathName = path.join(curPathName, pcs[i]);
+        
+        $ret = $ret.then($readDir.bind(null, curPathName)).then(function() {
+          console.log('Dir ' + pathName + ' already exists');
+        }).fail(function() {
+          console.log('Dir ' + pathName + ' is MISSING; creating...');
+          return $createDir(pathName).then(function() { console.log('Created!'); });
+        });
+        
+      }
+      
+      return $ret.then(function() {
         console.log('Ensured: ' + pathName);
+      });
+      
+    };
+    var $ensureFile = function(pathName, content) {
+      if (!U.exists(content)) content = '';
+      
+      // TODO: What if the file is huge?? Don't read it; instead check if it exists
+      return $readFile(pathName).fail(function() {
+        var pcs = pathName.split(path.sep);
+        return $ensureDir(pcs.slice(0, pcs.length - 1).join(path.sep)).then($writeFile.bind(null, pathName, content));
       });
     };
     var $readFile = function(pathName) {
@@ -50,18 +79,18 @@ new PACK.pack.Package({ name: 'persist',
             } else if (ENVIRONMENT.type === 'openshift') {
               
               this.pathName = path.join(ENVIRONMENT.fileRootName, this.packageName, 'state.json');
-              console.log('OPENSHIFT PERSIST PATH: ' + this.pathName);
+              console.log('Persister (openshift) at: ' + this.pathName);
               
             }
             
           },
           $init: function() {
             
+            return $ensureFile(this.pathName, '{}');
+            
+            /*
             var pcs = this.pathName.split(path.sep);
-            
-            var $ret = p.$null;
-            
-            var pathName = pcs[0];
+            return $ensureDir(pcs.slice(0, pcs.length - 1).join(path.sep)).then(function() { return $writeFile(pathName, '{}'); });
             
             for (var i = 1, len = pcs.length - 1; i < len; i++) // Subtract one to exclude the .json component
               $ret = $ret.then($ensureDir.bind(null, pathName = path.join(pathName, pcs[i])));
@@ -71,8 +100,10 @@ new PACK.pack.Package({ name: 'persist',
               return $writeFile(pathName, '{}');
               
             }).then(function() {
+              
               console.log('WROTE {}!!!!!!!!!!!');
-            });
+              
+            });*/
             
           },
           $getData: function() {
